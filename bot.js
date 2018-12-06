618a94091dd669c2ae873608cdeb725a937cbdd5
ekomekoteko <45575162+ekomekoteko@users.noreply.github.com>
2018-12-06 10:33:58 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,28 +1,16 @@
- const Discord = require('discord.js');
-const client = new Discord.Client();
+const Discord = require('discord.js');
+const A7MD = new Discord.Client();
 
 console.log("BOT ONLINE");
  
-client.on("guildMemberAdd", member => {
+A7MD.on("guildMemberAdd", member => {
   member.createDM().then(function (channel) {
-  return channel.send(`
+  return channel.send(` 
 **
-انفايد ريورد نااار 
-10 عضو = :smile: 
-``100 k credits ``
-https://discord.gg/mY5V8r
-**
-**
-https://discord.gg/EjsSZY
-سيرفر شوب يبيع كل شىء حتى بوتات الهاكر :wink: 
-**
-**
-https://discord.gg/aH8pJF
-سيرفر اكواد وتعليم عمل بوت هاكرز وبوتات :heart: 
-**
+سلام عليكم 
+لو سمحت ممكن تدخل سيرفرى
+                               [https://discord.gg/mY5V8r] 
 الدعووة خاصة لك ... [ ${member}  ]
 **`) 
 }).catch(console.error)
 })
-
-client.login(process.env.BOT_TOKEN);
